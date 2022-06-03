bd6ac47c39a4e122751d742062acdc3190e6e60e
ymy <icheeringsoul@gmail.com>
2022-06-03 16:34:22 +0800

    [delete] remove some extra code

--- mmm/management/command.py
+++ mmm/management/command.py
@@ -11,8 +11,6 @@ def cli(): ...
 
 @click.command()
 def start_order_executor():
-    print(1)
-    return
     OrderExecutor().create_task()
     asyncio.get_event_loop().run_forever()
 
@@ -21,26 +19,24 @@ def start_order_executor():
 @click.option('--names', '-n', required=True, type=click.Choice(['okex', 'binance']), multiple=True)
 @click.option('--topic', '-t', required=True, type=str)
 def start_data_source(name, topic):
-    print(2)
-    return
     if name == 'okex':
         OkexWsDatasource().subscribe(topic)
     elif name == 'binance':
-        ...
+        """todo"""
     asyncio.get_event_loop().run_forever()
 
 
 @click.command()
 @click.option('--name', '-n', required=True)
 def start_strategy(name):
-    ...
+    """todo"""
 
 
 @click.command()
 @click.option('--host', '-h', default='127.0.0.1')
 @click.option('--port', '-p', default='8888')
 def start_admin(host, port):
-    ...
+    """todo"""
 
 
 cli.add_command(start_order_executor)
